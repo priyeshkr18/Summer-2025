97bde0ee8f2a1cac3d8568f8410b2ac011b77b2d
Priyesh Choudhary <143314183+priyeshkr18@users.noreply.github.com>
2025-06-20 11:54:32 +0530

    Add files via upload

---
+++ varibales.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "Priyesh Choudhary" << endl;
+	cout << "I am 22 year old" << endl;
+	cout << "I live in Bihar" << endl;
+
+	return 0;
+}
